de3c34f8cacff15ac4a6c45350e0c135dda63da7
NathaNN73 <105460841+NathaNN73@users.noreply.github.com>
2023-10-18 00:21:21 -0500

    Add files via upload

---
+++ combinatorics/binomial_coefficient.cpp
@@ -0,0 +1,7 @@
+int C(int n, int k) {
+    double res = 1;
+    for (int i = 1; i <= k; ++i)
+        res = res * (n - k + i) / i;
+        
+    return (int)(res + 0.01);
+}
---
+++ combinatorics/catalan_numers_modN.cpp
@@ -0,0 +1,16 @@
+const int MOD = ... // example: 1000000007
+const int MAX = n
+int catalan[MAX];
+
+void init() {
+    catalan[0] = catalan[1] = 1;
+    for (int i=2; i<=n; i++) {
+        catalan[i] = 0;
+        for (int j=0; j < i; j++) {
+            catalan[i] += (catalan[j] * catalan[i-j-1]) % MOD;
+            if (catalan[i] >= MOD) {
+                catalan[i] -= MOD;
+            }
+        }
+    }
+}
